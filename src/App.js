7be75856b9a39261405a83df98b7cc68279ffb7a
laiba ahsan <laiba.ahsan27@gmail.com>
2024-06-12 00:11:21 +0530

    change in route in app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-import HERO from './components/hero/Hero';
-import ABOUT from './components/about/about';
-import FOOTER from './components/footer/footer'
-import PROJECTS from './components/Projects'
+import HERO from './components/hero/Hero.jsx';
+import ABOUT from './components/about/about.jsx';
+import FOOTER from './components/footer/footer,jsx'
+import PROJECTS from './components/Projects.jsx'
 
 function App() {
   return (
