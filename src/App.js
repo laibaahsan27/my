7ae3390c3deb022aae38d6e6cead8bd177c0a84a
laiba ahsan <laiba.ahsan27@gmail.com>
2024-06-12 00:30:45 +0530

    , to .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import HERO from './components/hero/Hero1.jsx';
 import ABOUT from './components/about/about.jsx';
-import FOOTER from './components/footer/footer,jsx'
+import FOOTER from './components/footer/footer.jsx'
 import PROJECTS from './components/Projects/index.jsx'
 
 function App() {
